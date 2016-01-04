fc73efb772264fccb7a88add4abcc2f203440d9d
pdomitrovic <petra.domitrovic@gmail.com>
2016-01-04 23:56:28 +0100

    Second commit
    Recursion part is done. Inducing SA remains to be done.

--- sa-ds.py
+++ sa-ds.py
@@ -1,7 +1,7 @@
 #getting the L and S types for each character
 def LSTypeChar(S, t, n):
     for x in xrange(n-1, -1, -1):
-        if S[x] == '$':
+        if ord(S[x]) == 0:
             t[x] = 1
         elif S[x] < S[x+1]:
             t[x] = 1
@@ -13,9 +13,9 @@ def LSTypeChar(S, t, n):
 def dCriticalChars(S, t, n, p1, d):
     i = -1
     j = 0
-    while (i < len(S) - 1):
+    while (i < n-1):
         isLMS = 0
-        for h in xrange(2, (d + 1) + 1):
+        for h in xrange(2, (d+1) + 1):
             if (t[i + h - 1] == 0) and (t[i + h] == 1):
                 isLMS = 1
                 break
@@ -26,13 +26,14 @@ def dCriticalChars(S, t, n, p1, d):
             i = i + h
         else:
             i = i + d
-        p1.append(i)
+        p1[j] = i
         j = j + 1
     return j
 
-#bucket sort
-#L/S bucket
+#L/S bucket sort
 def bucketSortLS(src, dest, S, t, n, n1, h):
+    #print 'Src: ', src
+    #print 'Dest: ', dest
     start = 0
     end = n1 - 1
     for i in xrange(0, n1):
@@ -47,7 +48,7 @@ def bucketSortLS(src, dest, S, t, n, n1, h):
             start = start + 1
     return
 
-#sort by d-critical substrings
+#d-critical substrings bucket sort
 def bucketSort(src, dest, S, t, n, n1, K, c, d):
     v_sum = 0
     for i in xrange(K+1):
@@ -69,74 +70,132 @@ def bucketSort(src, dest, S, t, n, n1, K, c, d):
         c[ord(S[j])] += 1
     return
 
-#main
-S = ('m', 'm', 'i', 'i', 's', 's', 'i', 'i', 's', 's', 'i', 'i', 'p', 'p', 'i',
-     'i', '$')
-t = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-SA = [] #result
-SA1 = []
-s1 = []
-d = 2
-n = len(S)
-K = 255
-bkt = []
-for i in xrange(K+1):
-    bkt.append(0)
+#recursive function - core of SA_DS algorithm
+def SA_DS(S, SA, n, K):
+    d = 2
+    print 'Pocetno stanje'
+    print '-------------------------------'
+    print 'S: ', S
+    print 'SA: ', SA
+    print 'n: ', n
+    print 'K: ', K
+    
+    #LS categorization
+    t = [0] * n
+    LSTypeChar(S, t, n)
+    print 't: ', t
+    
+    #d-critical characters
+    SA1 = SA
+    n1 = dCriticalChars(S, t, n, SA1, d)
+    SA1 = SA1[0:n1]
 
-LSTypeChar(S, t, n)
-n1 = dCriticalChars(S, t, n, SA1, d)
+    print 'SA1: ', SA1
+    print '-------------------------------'
 
-print S
-print t
-print SA1
+    #bucket sorting
+    s1 = SA[n-n1 :]
+    print 's1: ', s1
+    print '-------------------------------'
+    bkt = [0] * (K+1)
+    bucketSortLS(SA1, s1, S, t, n, n1, 3)  # 3 = d + 2 - 1
+    print 'LS pass: ', s1
+    bucketSort(s1, SA1, S, t, n, n1, K, bkt, 3)
+    print 'Pass 1: ', SA1
+    bucketSort(SA1, s1, S, t, n, n1, K, bkt, 2)
+    print 'Pass 2: ', s1
+    bucketSort(s1, SA1, S, t, n, n1, K, bkt, 1)
+    print 'Pass 3: ', SA1
+    bucketSort(SA1, s1, S, t, n, n1, K, bkt, 0)
+    print 'Pass 4: ', s1
+    print '-------------------------------'
+    del bkt
 
-s1 = list(SA1)
-bucketSortLS(s1, SA1, S, t, n, n1, 3)  # 3 = d + 2 - 1
-print SA1
-bucketSort(SA1, s1, S, t, n, n1, K, bkt, 3)
-print 'Pass 1: ', s1
-bucketSort(s1, SA1, S, t, n, n1, K, bkt, 2)
-print 'Pass 2: ', SA1
-bucketSort(SA1, s1, S, t, n, n1, K, bkt, 1)
-print 'Pass 3: ', s1
-bucketSort(s1, SA1, S, t, n, n1, K, bkt, 0)
-print 'Pass 4: ', SA1
-del bkt
+    SA[0:n1] = s1
+    SA[-n1:] = SA1
+    #print 'SA: ', SA
 
-SA = [None] * n
-for i in xrange(n1-1, -1, -1):
-    j = 2 * i
-    SA[j] = SA1[i]
-    SA[j+1] = -1
-for i in xrange(2*(n1-1)+3, n, 2):
-    SA[i] = -1
+    #naming
+    for i in xrange(n1-1, -1, -1):
+        j = 2 * i
+        SA[j] = SA1[i]
+        SA[j+1] = -1
+    for i in xrange(2*(n1-1) + 3, n, 2):
+        SA[i] = -1
 
-print 'SA: ', SA
-print 'SA1: ', SA1
+    #print 'SA: ', SA
+    #print 'SA1: ', SA1
 
-name = 0
-c = [-1, -1, -1, -1, -1]
-for i in xrange(n1):
-    pos = SA[2*i]
-    diff = 0
-    for h in xrange(4):
-        if ord(S[pos+h]) != c[h]:
-            diff = 1
-            break
-    if (ord(S[pos+4])*2 + t[pos+4]) != c[4]:
-        diff = 1
-    if diff == 1:
-        name += 1
-        for h in xrange(4):
-            if (pos+h) < n:
-                c[h] = ord(S[pos+h])
+    name = 0
+    c = [-1, -1, -1, -1]
+    for i in xrange(n1):
+        pos = SA[2*i]
+        diff = 0
+        #print 'I: ', i
+        #print 'C: ', c
+        #print 'S: ', S[pos : pos+4]
+        for h in xrange(3):
+            if pos+h < n:
+                if ord(S[pos+h]) != c[h]:
+                    #print 'Razlika1: ', ord(S[pos+h]), ' != ', c[h]
+                    diff = 1
+                    break
             else:
-                c[h] = -1
-        if (pos+h) < n:
-            ch[h] = ord(S[pos+h])*2 + t[pos+h]
+                if c[h] != -1:
+                    diff = 1
+                    break
+        if pos+3 < n:
+            if (ord(S[pos+3])*2 + t[pos+3]) != c[3]:
+                #print 'Razlika2: ', (ord(S[pos+3])*2 + t[pos+3]), ' != ', c[3]
+                diff = 1
         else:
-            c[h] = -1
-    if (pos%2 == 0):
-        pos -= 1
-    SA[pos] = name - 1
-print 'SA: ', SA
+            if c[3] != -1:
+                diff = 1
+        if diff == 1:
+            name += 1
+            for h in xrange(3):
+                if (pos+h) < n:
+                    c[h] = ord(S[pos+h])
+                else:
+                    c[h] = -1
+            if (pos+3) < n:
+                c[3] = ord(S[pos+3])*2 + t[pos+3]
+            else:
+                c[3] = -1
+        if (pos%2 == 0):
+            pos -= 1
+        SA[pos] = name - 1
+    #print 'SA: ', SA
+
+    #filling in the gaps in SA
+    i = n/2*2 - 1
+    j = n - 1
+    while (i >= 0 & i >= 0):
+        if SA[i] != -1:
+            SA[j] = SA[i]
+            j -= 1
+        i -= 2
+    s1 = SA[n-n1 :]
+    
+    #solving reduced problem
+    #print 'SA: ', SA
+    print 's1: ', s1
+    #print 'SA1: ', SA1
+
+    print'\n'
+    for i in xrange(n1):
+        s1[i] = chr(s1[i])
+    if name < n1:
+        SA_DS(s1, SA1, n1, name-1)
+    
+    
+
+
+#main
+K = 255
+S = ('m', 'm', 'i', 'i', 's', 's', 'i', 'i', 's', 's', 'i', 'i', 'p', 'p', 'i',
+     'i', chr(0))
+n = len(S)
+SA = [0] * n #result
+
+SA_DS(S, SA, n, K)
