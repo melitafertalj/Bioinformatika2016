6e500bed45e115e90c03bbb80b0cfa02d2d20d17
pdomitrovic <petra.domitrovic@gmail.com>
2016-01-13 14:23:35 +0100

    Added time and accuracy analysis.

--- sa-ds.py
+++ sa-ds.py
@@ -1,7 +1,9 @@
+import time
+
 #getting the L and S types for each character
 def LSTypeChar(S, t, n):
     for x in xrange(n-1, -1, -1):
-        if ord(S[x]) == 0:
+        if S[x] == 0:
             t[x] = 1
         elif S[x] < S[x+1]:
             t[x] = 1
@@ -57,7 +59,7 @@ def bucketSort(src, dest, S, t, n, n1, K, c, d):
         j = src[i] + d
         if (j > n-1):
             j = n - 1
-        c[ord(S[j])] += 1
+        c[S[j]] += 1
     for i in xrange(K+1):
         v_len = c[i]
         c[i] = v_sum
@@ -66,8 +68,8 @@ def bucketSort(src, dest, S, t, n, n1, K, c, d):
         j = src[i] + d
         if (j > n-1):
             j = n - 1
-        dest[c[ord(S[j])]] = src[i]
-        c[ord(S[j])] += 1
+        dest[c[S[j]]] = src[i]
+        c[S[j]] += 1
     return
 
 #compute start/end of buckets
@@ -76,7 +78,7 @@ def getBuckets(S, bkt, n, K, end):
     for i in xrange(K+1):
         bkt[i] = 0
     for i in xrange(n):
-        bkt[ord(S[i])] += 1
+        bkt[S[i]] += 1
     for i in xrange(K+1):
         v_sum += bkt[i]
         if end == 1:
@@ -87,42 +89,47 @@ def getBuckets(S, bkt, n, K, end):
 
 #recursive function - core of SA_DS algorithm
 def SA_DS(S, SA, n, K):
-    d = 2
-    print 'Beginning'
-    print '-------------------------------'
-    print 'S: ', S
-    print 'SA: ', SA
-    print 'n: ', n
-    print 'K: ', K
+    #print 'Beginning'
+    #print '-------------------------------'
+    #print 'S: ', S
+    #print 'SA: ', SA
+    #print 'n: ', n
+    #print 'K: ', K
     
     #LS categorization
     t = [0] * n
     LSTypeChar(S, t, n)
-    print 't: ', t
+    #print 't: ', t
     
     #d-critical characters
     SA1 = SA
-    n1 = dCriticalChars(S, t, n, SA1, d)
+    n1 = dCriticalChars(S, t, n, SA1, 2) #d = 2
     SA1 = SA1[0:n1]
 
-    print 'SA1: ', SA1
+    #print 'SA1: ', SA1
+    #print 'D-critical characters found.'
     
     #bucket sorting
     s1 = SA[n-n1 :]
-    print 's1: ', s1
-    print '-------------------------------'
+    #print 's1: ', s1
+    #print '-------------------------------'
     bkt = [0] * (K+1)
     bucketSortLS(SA1, s1, S, t, n, n1, 3)  # 3 = d + 2 - 1
-    print 'LS pass: ', s1
+    #print 'LS pass: ', s1
+    #print 'Bucket sort LS done.'
     bucketSort(s1, SA1, S, t, n, n1, K, bkt, 3)
-    print 'Pass 1: ', SA1
+    #print 'Pass 1: ', SA1
+    #print 'Bucket sort pass 1 done.'
     bucketSort(SA1, s1, S, t, n, n1, K, bkt, 2)
-    print 'Pass 2: ', s1
+    #print 'Pass 2: ', s1
+    #print 'Bucket sort pass 2 done.'
     bucketSort(s1, SA1, S, t, n, n1, K, bkt, 1)
-    print 'Pass 3: ', SA1
+    #print 'Pass 3: ', SA1
+    #print 'Bucket sort pass 3 done.'
     bucketSort(SA1, s1, S, t, n, n1, K, bkt, 0)
-    print 'Pass 4: ', s1
-    print '-------------------------------'
+    #print 'Pass 4: ', s1
+    #print 'Bucket sort pass 4 done.'
+    #print '-------------------------------'
     del bkt
 
     SA[0:n1] = SA1
@@ -143,7 +150,7 @@ def SA_DS(S, SA, n, K):
         diff = 0
         for h in xrange(3):
             if pos+h < n:
-                if ord(S[pos+h]) != c[h]:
+                if S[pos+h] != c[h]:
                     diff = 1
                     break
             else:
@@ -151,7 +158,7 @@ def SA_DS(S, SA, n, K):
                     diff = 1
                     break
         if pos+3 < n:
-            if (ord(S[pos+3])*2 + t[pos+3]) != c[3]:
+            if (S[pos+3]*2 + t[pos+3]) != c[3]:
                 diff = 1
         else:
             if c[3] != -1:
@@ -160,11 +167,11 @@ def SA_DS(S, SA, n, K):
             name += 1
             for h in xrange(3):
                 if (pos+h) < n:
-                    c[h] = ord(S[pos+h])
+                    c[h] = S[pos+h]
                 else:
                     c[h] = -1
             if (pos+3) < n:
-                c[3] = ord(S[pos+3])*2 + t[pos+3]
+                c[3] = S[pos+3]*2 + t[pos+3]
             else:
                 c[3] = -1
         if (pos%2 == 0):
@@ -182,17 +189,13 @@ def SA_DS(S, SA, n, K):
     s1 = SA[n-n1 :]
     
     #solving reduced problem
-    print 's1: ', s1, '\n'
+    #print 's1: ', s1, '\n'
     
     #recursion
     if name < n1:
-        print '\nRecursion:\n'
-        for i in xrange(n1):
-            s1[i] = chr(s1[i])
+        #print '\nRecursion:\n'
         SA_DS(s1, SA1, n1, name-1)
-        for i in xrange(n1):
-            s1[i] = ord(s1[i])
-        print '\nRecursion ended.\n\n'
+        #print '\nRecursion ended.\n\n'
     else:
         for i in xrange(n1):
             SA1[s1[i]] = i
@@ -200,8 +203,9 @@ def SA_DS(S, SA, n, K):
     SA[0:n1] = s1
     SA[-n1:] = SA1
     
-    print 'SA1: ', SA1
-    dCriticalChars(S, t, n, s1, d)
+    #print 'SA1: ', SA1
+    #print 'Inducing SA:'
+    dCriticalChars(S, t, n, s1, 2)
     SA[-n1:] = s1
     bkt = [0] * (K+1)
     getBuckets(S, bkt, n, K, 1)
@@ -214,23 +218,26 @@ def SA_DS(S, SA, n, K):
         j = SA[i]
         SA[i] = -1
         if j > 0 and t[j] == 1 and t[j-1] == 0:
-            bkt[ord(S[j])] -= 1
-            SA[bkt[ord(S[j])]] = j
-    print 'Step 1: ', SA
+            bkt[S[j]] -= 1
+            SA[bkt[S[j]]] = j
+    #print 'Step 1: ', SA
+    #print 'Step 1 done.'
     getBuckets(S, bkt, n, K, 0)
     for i in xrange(n):
         j = SA[i] - 1
         if j >= 0 and t[j] == 0:
-            SA[bkt[ord(S[j])]] = j
-            bkt[ord(S[j])] += 1
-    print 'Step 2: ', SA
+            SA[bkt[S[j]]] = j
+            bkt[S[j]] += 1
+    #print 'Step 2: ', SA
+    #print 'Step 2 done.'
     getBuckets(S, bkt, n, K, 1)
     for i in xrange(n - 1, -1, -1):
         j = SA[i] - 1
         if j >= 0 and t[j] == 1:
-            bkt[ord(S[j])] -= 1
-            SA[bkt[ord(S[j])]] = j
-    print 'Step 3: ', SA
+            bkt[S[j]] -= 1
+            SA[bkt[S[j]]] = j
+    #print 'Step 3: ', SA
+    #print 'Step 3 done.'
     
     del bkt, t, SA1, s1
 
@@ -238,20 +245,32 @@ def SA_DS(S, SA, n, K):
 #main
 K = 255
 
-# 'otorinolaringologija'
-#S = ('o', 't', 'o', 'r', 'i', 'n', 'o', 'l', 'a', 'r', 'i', 'n', 'g', 'o', 'l',
-#     'o', 'g', 'i', 'j', 'a', chr(0))
+with open("Tests/haemo.txt", "r") as inputFile:
+    lines = inputFile.read().splitlines()
 
-# 'mmiissiissiippii'
-#S = ('m', 'm', 'i', 'i', 's', 's', 'i', 'i', 's', 's', 'i', 'i', 'p', 'p', 'i',
-#     'i', chr(0))
-
-# 'gle malu vocku poslije kise, puna je kapi pa ih njise'
-S = ('g', 'l', 'e', ' ', 'm', 'a', 'l', 'u', ' ', 'v', 'o', 'c', 'k', 'u', ' ',
-     'p', 'o', 's', 'l', 'i', 'j', 'e', ' ', 'k', 'i', 's', 'e', ',', ' ', 'p',
-     'u', 'n', 'a', ' ', 'j', 'e', ' ', 'k', 'a', 'p', 'i', ' ', 'p', 'a', ' ',
-     'i', 'h', ' ', 'n', 'j', 'i', 's', 'e', chr(0))
+S = []
+for line in lines:
+    if line[0] != '>':
+        S.extend(line)
+for i in xrange(len(S)):
+    S[i] = ord(S[i])
+S.append(0)
 
 n = len(S)
 SA = [0] * n #result
+
+start = time.clock()
 SA_DS(S, SA, n, K)
+end = time.clock()
+print 'Time elapsed (in seconds): ', end - start
+
+with open("output.txt", "w") as outputFile:
+    outputFile.write(', '.join(str(x) for x in SA))
+
+strS = ''.join(str(x) for x in S)
+isSorted = 'Sorted!'
+for i in xrange(1, len(SA)):
+    if S[SA[i-1] :] > S[SA[i] :]:
+        isSorted = 'Not sorted! ' + str(i) + '. suffix is smaller than ' + str(i+1) + '.'
+        break
+print isSorted
